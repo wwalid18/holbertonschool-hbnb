f49adf08f765e2da23e5633c20fe8cad04d30bd6
NourKasmi <nourkasmi@gmail.com>
2025-03-13 12:54:56 +0100

    again

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,3 +1,9 @@
+from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
+
 class HBnBFacade:
     """Facade to manage users, places, reviews, and amenities."""
 
