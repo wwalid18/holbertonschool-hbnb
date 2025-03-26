905ab277d586ba6bb837a3cbade63528c6054015
NourKasmi <nourkasmi@gmail.com>
2025-03-26 04:51:13 +0100

    user

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask, jsonify
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
