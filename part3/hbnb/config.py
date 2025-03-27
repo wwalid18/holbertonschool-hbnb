365790ade9eca14911f0c24de75585c019851bd3
NourKasmi <nourkasmi@gmail.com>
2025-03-27 16:42:31 +0100

    reviews.py

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -1,8 +1,11 @@
+# config.py
 import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hbnb.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
